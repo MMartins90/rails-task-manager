2f40691eecfae97172d5bc029bd1ad9d52b619b8
Marco Martins <marco_martins909@hotmail.co.uk>
2020-05-12 18:19:07 +0100

    Completed task 2

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
   get "tasks/new", to: "tasks#new", as: :new
   get "tasks/:id", to: "tasks#show", as: :task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
   post "tasks", to: "tasks#create"
+  patch "tasks/:id", to: "tasks#update"
+  delete "task/:id", to: "tasks#destroy"
 end
